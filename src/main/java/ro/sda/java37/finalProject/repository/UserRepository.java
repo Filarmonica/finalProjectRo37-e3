5d53b7b8e451d6836c9d0002f247895bcbc3eac0
GiuliaDD <giulia.diaconescu@gmail.com>
2022-08-16 21:07:37 +0300

    findUserByEmail method added

--- src/main/java/ro/sda/java37/finalProject/repository/UserRepository.java
+++ src/main/java/ro/sda/java37/finalProject/repository/UserRepository.java
@@ -3,5 +3,11 @@ package ro.sda.java37.finalProject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ro.sda.java37.finalProject.entities.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+  List<User> findAll();
+
+  Optional<User> findUserByEmail(String email);
 }
