196fc5655341741a5dde43c36b91ca6c2635655c
GiuliaDD <giulia.diaconescu@gmail.com>
2022-08-16 19:18:41 +0300

    Main updated

--- src/main/java/ro/sda/java37/finalProject/FinalProjectApplication.java
+++ src/main/java/ro/sda/java37/finalProject/FinalProjectApplication.java
@@ -1,16 +1,12 @@
 package ro.sda.java37.finalProject;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ro.sda.java37.finalProject.repository.UserRepository;
-import ro.sda.java37.finalProject.services.ProductService;
-import ro.sda.java37.finalProject.services.UserService;
 
 @SpringBootApplication
 public class FinalProjectApplication {
 
-    public static void main(String[] args) {
-      SpringApplication.run(FinalProjectApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(FinalProjectApplication.class, args);
+  }
 }
